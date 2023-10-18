81b9885b5003eeba992230bb80ffad12105c4c9a
tahlilislam <tahlilislam@gmail.com>
2023-10-18 11:10:46 -0700

    completed 24.4 step 8 by adding Flask session

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, flash
+from flask import Flask, render_template, request, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey as survey
 
@@ -11,9 +11,13 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
 
-# Initialize responses as an empty list to store user answers
-responses = []
+# # Initialize responses as an empty list to store user answers
+# responses = []
 
+# key names will use to store some things in the session;
+# put here as constants so we're guaranteed to be consistent in
+# our spelling of these
+RESPONSES_KEY = "responses"
 
 @app.route('/')
 def start_surver():
@@ -24,12 +28,16 @@ def start_surver():
 @app.route("/begin", methods=["POST"])
 def start_survey():
     """Clear the session of responses."""
+    session[RESPONSES_KEY] = []
 
     return redirect("/questions/0")
 
 
 @app.route('/questions/<int:q_id>')
 def show_question(q_id):
+
+    responses = session.get(RESPONSES_KEY)
+
     if (responses is None):
         # trying to access question page too soon
         return redirect("/")
@@ -55,7 +63,11 @@ def handle_question():
     """Save response and redirect to next question."""
 
     final_choice = request.form["answer"]
+
+    # get the updated response list from session, modify it, and update the list to session again
+    responses = session[RESPONSES_KEY]
     responses.append(final_choice)
+    session[RESPONSES_KEY] = responses
 
     if (len(responses) == len(survey.questions)):
         # They've answered all the questions! Thank them.
